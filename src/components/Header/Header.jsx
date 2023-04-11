d2019bec4e57c2963189503c2a9504086d754126
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-12 03:28:47 +0600

    Header Complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-base-100 lg:mx-12">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/statistics">Satistics</Link></li>
+                        <li><Link to="/appliedjob">Applied Jobs</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                    </ul>
+                </div>
+                <Link to="" className="normal-case text-2xl font-bold">JobHunter</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link to="/statistics">Statistics</Link></li>
+                    <li><Link to="/appliedjob">Applied Jobs</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link to="/applynow" className="btn bg-blue-500 text-white border-none hover:bg-blue-600">Start Applying</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Layout from './components/Layout/Layout';
+import Statistic from './components/Statistics/Statistic';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Layout></Layout>
+      },
+      {
+        path: "/statistics",
+        element: <Statistic></Statistic>
+      },
+      {
+        path: "/appliedjob",
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
+    ]
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
