7e7a3aff27394271c1d13e051016267c1860ce9c
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-12 22:01:07 +0600

    Complete this project

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
                 <img src={img} className="max-w-sm  -my-4 w-10/12 md:w-full" />
                 <div className='p-0 md:pr-96'>
                     <h1 className="text-5xl font-bold leading-snug">Find your<br />Dream job<br /><span className='text-blue-400'>from JOBHUNTER</span></h1>
-                    <p className="py-6">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <p className="py-6">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. You can find all updated job here.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
