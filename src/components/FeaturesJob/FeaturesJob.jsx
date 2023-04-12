89a78e9d8a2d727c286d56a1ca565de5bbfe7773
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-12 07:19:52 +0600

    Feature Jobs added

--- src/components/FeaturesJob/FeaturesJob.jsx
+++ src/components/FeaturesJob/FeaturesJob.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Jobs from '../Jobs/Jobs';
 
 const FeaturesJob = () => {
+    const [featuresJob, setFeaturesJobs] = useState([])
+    const [showAllJobs, setShowAllJobs] = useState(false);
+
+    useEffect(() => {
+        fetch('fetauresJobs.json')
+        .then(res => res.json())
+        .then(data => setFeaturesJobs(data))
+    }, [])
+    
+    const handleViewAll = () => {
+        setShowAllJobs(!showAllJobs);
+    }
+    
+    const jobsToShow = showAllJobs ? featuresJob : featuresJob.slice(0, 4);
+
     return (
-        <div>
-            hi
+        <div className='mb-48'>
+            <div className='text-center'>
+                <h1 className='text-5xl font-bold text-slate-950  mt-6'>Featured Jobs</h1>
+                <p className='text-gray-400 mt-4 mb-8'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        <div className='grid grid-cols-1 mx-auto md:w-10/12 md:grid-cols-2 gap-6'>
+            {
+                jobsToShow.map(job => <Jobs key={job.companyName} job={job}></Jobs>)
+            }
+        </div>
+        <button onClick={handleViewAll} className='btn mt-10 mx-auto flex justify-center  bg-blue-500 text-white border-none hover:bg-blue-800'>{showAllJobs ? 'Show Less' : 'View All'}</button>
         </div>
     );
 };
---
+++ src/components/Jobs/Jobs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+const Jobs = ({ job }) => {
+    console.log(job)
+    const { companyImage, jobTitle, companyName, jobType, location, salary } = job
+    return (
+        <div className='border-2 p-8'>
+            <img className='w-36' src={companyImage} alt="" />
+            <h2 className='text-2xl font-bold mt-8 text-slate-600'>{jobTitle}</h2>
+            <p className='text-slate-400 my-2'>{companyName}</p>
+            <div className='flex gap-3'>
+                {
+                    jobType.map(type => <p className='border-2 px-3 py-1 border-blue-500 text-blue-500'>{type}</p>)
+                }
+            </div>
+            <div className='flex my-3'>
+                <p className='text-slate-500 mr-3'><FontAwesomeIcon icon={faLocationDot} className='mr-2'/>{location}</p>
+                <p className='text-slate-500'>Salary: {salary}</p>
+            </div>
+            <button className='btn bg-blue-500 text-white border-none hover:bg-blue-800'>View Details</button>
+        </div>
+    );
+};
+
+export default Jobs;
