d5874052842309265cd58ba2b529564174e8b80b
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-12 21:34:49 +0600

    Fixing Some issues

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
             <div className="hero-content  flex-col lg:flex-row-reverse">
                 <img src={img} className="max-w-sm  -my-4 w-10/12 md:w-full" />
                 <div className='p-0 md:pr-96'>
-                    <h1 className="text-5xl font-bold leading-snug">One Step<br />CloserTo Your<br /><span className='text-blue-400'>Dream Job</span></h1>
+                    <h1 className="text-5xl font-bold leading-snug">Find your<br />Dream job<br /><span className='text-blue-400'>from JOBHUNTER</span></h1>
                     <p className="py-6">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
--- src/components/Jobdetails/JobDetails.jsx
+++ src/components/Jobdetails/JobDetails.jsx
@@ -13,7 +13,7 @@ const JobDetails = () => {
             setJobs(checkData);
         }
     }, [id])
-    const {description,location,jobTitle, educationalRequirement, experience, jobResponsibility, phone,salary} = jobs;
+    const {description,location,jobTitle, educationalRequirement, experience, jobResponsibility, email, phone,salary} = jobs;
 
     return (
         <div>
@@ -23,21 +23,21 @@ const JobDetails = () => {
             </div>
         <div className='md:flex mx-auto w-10/12 info my-24'>
             <div className='md:w-9/12 pr-12 '>
-                <p className='mt-6'><span>Job Description:</span> {description}</p>
-                <p className='mt-6'><span>Job Responsibility:</span>{jobResponsibility}</p>
-                <p className='mt-6'><span>Educational requirement:</span>{educationalRequirement}</p>
-                <p className='mt-6'><span>Experience:</span>{experience}</p>
+                <p className='mt-6'><span>Job Description: </span> {description}</p>
+                <p className='mt-6'><span>Job Responsibility: </span>{jobResponsibility}</p>
+                <p className='mt-6'><span>Educational requirement: <br/></span>{educationalRequirement}</p>
+                <p className='mt-6'><span>Experience: <br/></span>{experience}</p>
             </div>
             <div className='md:w-1/4 bg-blue-400 text-slate-200 p-8 leading-10 '>
                 <h3  className='font-xl font-bold'>Job Details</h3>
                 <hr />
-                <p><span className='text-slate-700'>Salary:</span>{salary}</p>
-                <p><span className='text-slate-700'>Job Title:</span>{jobTitle}</p>
+                <p><span className='text-slate-700'>Salary: </span>{salary}</p>
+                <p><span className='text-slate-700'>Job Title: </span>{jobTitle}</p>
                 <h3 className='font-xl font-bold'>Contact Information</h3>
                 <hr />
-                <p><span className='text-slate-700'>Phone:</span>{phone}</p>
-                <p><span className='text-slate-700'>Email:</span>xyz@yahoo.com</p>
-                <p><span>Address:</span>{location}</p>
+                <p><span className='text-slate-700'>Phone: </span>{phone}</p>
+                <p><span className='text-slate-700'>Email: </span>{email}</p>
+                <p><span className='text-slate-700'>Address: </span>{location}</p>
                 <button onClick={() => addToDb(id)} className='btn border-0 bg-white text-blue-600 px-4 py-1 w-full mt-4 rounded-md font-bold'>Apply Now</button>
             </div>
             
