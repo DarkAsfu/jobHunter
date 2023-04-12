87fe9b6012c208738a8cc20203fdd60d067626ca
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-04-12 06:17:36 +0600

    Feature job data making complete

---
+++ src/components/FeaturesJob/FeaturesJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturesJob = () => {
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+
+export default FeaturesJob;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import JobCatagories from '../JobCatagories/JobCatagories';
+import FeaturesJob from '../FeaturesJob/FeaturesJob';
 
 const Layout = () => {
     return (
         <div>
             <Hero></Hero>
             <JobCatagories></JobCatagories>
+            <FeaturesJob></FeaturesJob>
         </div>
     );
 };
